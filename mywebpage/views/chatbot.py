3758e8c33bdeb2b69e9732fea46da4825f665e0d
Johnson Njenga <110740726+Johnsonndungu@users.noreply.github.com>
2024-12-20 17:56:49 +0300

    chatbot.py

--- mywebpage/views/chatbot.py
+++ mywebpage/views/chatbot.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.title("Still in development")
+import streamlit.components.v1 as components
+
+# Embedding the chatbot using an HTML snippet
+chatbot_html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Embed Chatbot Script -->
+    <script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></script>
+<zapier-interfaces-chatbot-embed is-popup='false' chatbot-id='cm4wut83s00091423o4daisoa' height='600px' width='400px'></zapier-interfaces-chatbot-embed>
+</head>
+<body>
+    </script>
+</body>
+</html>
+"""
+
+# Display chatbot in Streamlit
+components.html(chatbot_html, height=600, scrolling=True)
